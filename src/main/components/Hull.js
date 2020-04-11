93291e0a83d66206d1b25cfaad21a5735ae9230d
Gabe Corso <gcorso@massiveu.com>
2020-04-11 00:13:34 -0400

    initial commit pirate radio ahoy

---
+++ src/main/components/Hull.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import '../../styles/Hull.css';
+
+
+class Hull extends Component {
+    render () {
+        return (
+            <div className={'hull-outer-container'}>
+                Ahoy matey, ye've stumbled upon some buried treasure...
+            </div>
+        );
+    }
+}
+
+export default Hull;
+
